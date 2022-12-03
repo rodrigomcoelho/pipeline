83950fc51c566de4d5a66a25483d040c9da05bde
Rodrigo Coelho <rodrigo.coelho@gruposbf.com.br>
2022-12-03 14:54:47 -0400

    create cicd pipeline

--- plugins/fisia/generic/utils/custom_print.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-# -------------------------------------------------------------
-def default_logging(*values: object, sep: str = " ", end: str = ""):
-    logging.info(
-        " " + (sep or " ").join([str(value) for value in values]) + (end or "")
-    )
